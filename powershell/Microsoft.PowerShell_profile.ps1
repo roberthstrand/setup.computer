0311d927d34ef6b7aadd919cc9b6da8a1f8fa76d
Roberth Strand <rs@destruktive.one>
2019-10-08 15:43:04 +0200

    Create Microsoft.PowerShell_profile.ps1
    Standard PowerShell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+Import-Module -Name posh-git
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+function prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    Write-Host
+    # Reset color, which can be messed up by Enable-GitColors
+    #$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    if (Test-Administrator) {  # Use different username if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+    Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor White
+    Write-Host "$ENV:COMPUTERNAME" -ForegroundColor DarkGray
+    if ($s -ne $null) {  # color for PSSessions
+        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+        Write-Host ") " -NoNewline -ForegroundColor DarkGray
+    }
+    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
+    #Write-Host (Get-Date -Format "dd/MM/yy - HH:mm") -NoNewline -ForegroundColor White
+    #Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-VcsStatus
+    Write-Host ""
+    return "# "
+}
