3756cd7de78e24fddcacd7cd733315b291ef22e9
Roberth Strand <roberth@strandkonsult.no>
2020-01-13 19:43:22 +0100

    Added Choco, structure

---
+++ setup.ps1
@@ -0,0 +1,30 @@
+<#
+This is a PowerShell script to set up my default environment.
+Feel free to use this file to get inspiration for your own script.
+
+@RoberthStrand (GitHub)
+@RoberthTweets (Twitter)
+
+#>
+
+# Install Chocolatey, the best package manager for Windows
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install selected packages
+$packages = @(
+    "vlc"
+    "7zip"
+    "git"
+    "putty"
+    "googlechrome"
+    "firefox"
+    "filezilla"
+    "teamviewer"
+    "keepass"
+    "vscode"
+    "terraform"
+    "rdmfree"
+    "discord"
+    "steam"
+)
+choco install $packages -y
